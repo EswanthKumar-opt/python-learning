c886bd27a1072230a034e8c8f4ec43ebc4c4ff4f
Eswanth Kumar V. <eswanth.kumar@optisolbusiness.com>
2024-09-18 15:08:19 +0530

    python learning

---
+++ Decorator.py
@@ -0,0 +1,23 @@
+##    Decorator
+
+## Its is used to call the function with the help of the another function argument
+
+
+def This_is_dec(func):
+    def inner_fn():
+        print("entering into the inner function")
+        func()
+        print("thanks for chosing decorator")
+    return inner_fn
+
+@This_is_dec ## object illama epadi use pannikalam
+def sample_fn():
+    print("hello world")
+
+# obj = This_is_dec(sample_fn) // normal way
+# obj()
+
+sample_fn()
+
+
+
